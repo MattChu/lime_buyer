82f84ce474128e9c2a8f7ab6605c7d2f07c88248
chuchu <chuchu@chuchus-MacBook-Pro.local>
2025-07-07 11:59:59 +0100

    create UserContext and wrap APP

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+Repo Setup Instructions WIP
 
+-- cd lime_buyer
+-- npm install
+-- add folder firebase and add your firebase.config file as firebase.js (firebase/firebase.js)
--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
+import { useContext } from "react";
+import { UserContext } from "./contexts/UserContext";
+
 function App() {
+  const { user, setuser } = useContext(UserContext);
   return <></>;
 }
 
