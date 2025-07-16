7540c50291e421c1f490aa0410b9c4be3e0d4ca9
chuchu <chuchu@chuchus-MacBook-Pro.local>
2025-07-16 10:00:21 +0100

    set navbar dashboard or login based on whether a user is logged in

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,21 +1,13 @@
 import { useContext } from "react";
+import { UserContext } from "../contexts/UserContext";
 import { Link } from "react-router-dom";
 
-import {
-  AppBar,
-  Toolbar,
-  Typography,
-  Grid,
-  Button,
-  Avatar,
-} from "@mui/material";
+import { AppBar, Toolbar, Typography, Grid, Button, Avatar } from "@mui/material";
 
 export const NavBar = () => {
-  // const { loggedInUser, setLoggedInUser } = useContext(UserContext);
+  const { user } = useContext(UserContext);
   return (
-    <AppBar
-      sx={{ mt: 0, mb: 1, position: "sticky", borderRadius: 0, boxShadow: 10 }}
-    >
+    <AppBar sx={{ mt: 0, mb: 1, position: "sticky", borderRadius: 0, boxShadow: 10 }}>
       <Toolbar>
         <Typography
           component="h1"
@@ -45,22 +37,28 @@ export const NavBar = () => {
           <Button color="inherit" component={Link} to="/Signup">
             Signup
           </Button>
-          <Button color="inherit" component={Link} to="/Login">
-            Login
-            <Avatar
-              sx={{
-                display: "flex",
-                alignContent: "center",
-                width: 24,
-                m: 1,
-                height: 24,
-                bgcolor: "white",
-              }}
-              // alt={`Avatar Image for ${loggedInUser?.username}`}
-              // src={`${loggedInUser?.avatar_url}`}
-              // title={`Currently logged in as ${loggedInUser.name}`}
-            />
-          </Button>
+          {user ? (
+            <Button color="inherit" component={Link} to="/dashboard">
+              Dashboard
+              <Avatar
+                sx={{
+                  display: "flex",
+                  alignContent: "center",
+                  width: 24,
+                  m: 1,
+                  height: 24,
+                  bgcolor: "white",
+                }}
+                // alt={`Avatar Image for ${loggedInUser?.username}`}
+                // src={`${loggedInUser?.avatar_url}`}
+                // title={`Currently logged in as ${loggedInUser.name}`}
+              />
+            </Button>
+          ) : (
+            <Button color="inherit" component={Link} to="/login">
+              Login
+            </Button>
+          )}
         </Grid>
       </Toolbar>
     </AppBar>
