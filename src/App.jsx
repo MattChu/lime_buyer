bf229ac99dac2e3d272735805c353ef6151d4be4
cnorman2000 <cnorman2000@gmail.com>
2025-07-17 10:04:50 +0100

    added edit profile page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import { useContext } from "react";
 import { UserContext } from "./contexts/UserContext";
 
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 
 import NavBar from "./components/NavBar";
 import Signup from "./pages/Signup";
@@ -10,6 +15,7 @@ import Dashboard from "./pages/Dashboard";
 import MapView from "./pages/MapViewer";
 import InputLocation from "./components/InputLocation";
 import Error from "./pages/Error";
+import EditProfile from "./pages/EditProfile";
 
 function App() {
   const { user } = useContext(UserContext);
@@ -18,11 +24,18 @@ function App() {
       <NavBar />
       <Routes>
         <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={!user ? <Login /> : <Navigate to="/dashboard" />} />
-        <Route path="/dashboard" element={user ? <Dashboard /> : <Navigate to="/login" />} />
+        <Route
+          path="/login"
+          element={!user ? <Login /> : <Navigate to="/dashboard" />}
+        />
+        <Route
+          path="/dashboard"
+          element={user ? <Dashboard /> : <Navigate to="/login" />}
+        />
         <Route path="/mapview" element={<MapView />} />
         <Route path="/lemons" element={<Error />} />
         <Route path="*" element={<Navigate to="/mapview" />} />
+        <Route path="/editprofile" element={<EditProfile />} />
       </Routes>
     </Router>
   );
