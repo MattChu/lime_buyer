753f277b9a363b4716a56314bd41aacf90cad0a2
SChung97 <steph9chung@hotmail.com>
2025-07-17 10:19:51 +0100

    addition of error messages when signing in with incorrect credentials

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,9 @@
 import { useState } from "react";
-import { signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import {
+  signInWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
 import { auth } from "../../firebase/firebase.js";
 import { useNavigate } from "react-router-dom";
 
@@ -9,19 +13,31 @@ auth.useDeviceLanguage();
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [isError, setIsError] = useState("");
   const navigate = useNavigate();
 
   const handleLogin = async (e) => {
     e.preventDefault();
+    setIsError("");
     try {
       await signInWithEmailAndPassword(auth, email, password);
       navigate("/dashboard");
     } catch (err) {
+      console.error(err);
       console.log(err.message);
+
+      if (err.code === "auth/invalid-credential") {
+        setIsError("Incorrect email and/or password");
+      } else if (err.code === "auth/missing-password") {
+        setIsError("Please enter password to continue");
+      } else if (err.code === "auth/invalid-email") {
+        setIsError("Please enter email to continue");
+      }
     }
   };
 
   const handleGoogleSignup = async () => {
+    setIsError("");
     try {
       await signInWithPopup(auth, provider);
       navigate("/dashboard");
@@ -34,11 +50,25 @@ function Login() {
     <>
       <form className="signup-container" onSubmit={handleLogin}>
         <h2 className="signup-title">Log In</h2>
-        <input className="signup-input" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
-        <input className="signup-input" placeholder="Password" type="password" onChange={(e) => setPassword(e.target.value)} />
-        <button className="signup-button" type="submit">Log In</button>
+        <input
+          className="signup-input"
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          className="signup-input"
+          placeholder="Password"
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button className="signup-button" type="submit">
+          Log In
+        </button>
+        {isError && <p className="Login-error">{isError}</p>}
       </form>
-      <button className="signup-button" onClick={handleGoogleSignup}>Sign Up with Google</button>
+      <button className="signup-button" onClick={handleGoogleSignup}>
+        Sign Up with Google
+      </button>
     </>
   );
 }
